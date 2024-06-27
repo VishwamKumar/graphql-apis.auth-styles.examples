8469ae5fdad79040362823910feb372a078994ce
Vishwam <vishwamkumar@outlook.com>
2024-06-27 15:49:28 -0400

    Initial commit

---
+++ GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using HotChocolate;
+global using HotChocolate.Resolvers;
+global using HotChocolate.Authorization;
+
+global using FirebaseAdmin;
+global using FirebaseAdminAuthentication.DependencyInjection.Extensions;
+
+//global using WeatherApp.GraphQLApi.OAuth2Firebase.Configs;
+global using WeatherApp.GraphQLApi.OAuth2Firebase.Dtos;
+global using WeatherApp.GraphQLApi.OAuth2Firebase.Schemas.Queries;
+
+
